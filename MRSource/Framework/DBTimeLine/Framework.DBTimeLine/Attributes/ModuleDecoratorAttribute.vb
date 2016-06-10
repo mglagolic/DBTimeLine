2b5320cbe91ba476e9fa46378b95a52bed740457
Miro Glagolić <miro.glagolic@gmail.com>
2016-06-10 22:54:44 +0200

    Module decorator

---
+++ MRSource/Framework/DBTimeLine/Framework.DBTimeLine/Attributes/ModuleDecoratorAttribute.vb
@@ -0,0 +1,12 @@
+﻿<AttributeUsage(AttributeTargets.Class, AllowMultiple:=False, Inherited:=False)>
+Public Class ModuleDecoratorAttribute
+    Inherits Attribute
+
+    Public Property ClassName As String
+    Public Property AssemblyName As String
+
+    Public Sub New(className As String, assemblyName As String)
+        Me.ClassName = className
+        Me.AssemblyName = assemblyName
+    End Sub
+End Class
